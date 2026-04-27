a26592df1013314c4291da8e416d9ab3f42ebbb3
SGHPK0905 <sonpropk@gmail.com>
2026-04-27 19:55:31 +0700

    sửa lại vòng lặp trong main về 500 và chạy lại kiểm thử

--- main.py
+++ main.py
@@ -216,8 +216,8 @@ def misoa(search_agents, max_iterations, lower_bound, upper_bound, dimension, ob
 # ==========================================
 if __name__ == "__main__":
     search_agents = 30 
-    fun_name = 'F9'  
-    max_iterations = 1000 
+    fun_name = 'F1'  
+    max_iterations = 500 
     
     lowerbound, upperbound, dimension, fitness = fun_info(fun_name)
     
